fee41d54a49e6ae8b032bde651f042f750c01d72
MARIAMJESTEM <kl2ag2@komp01.lo1cg.org>
2017-10-13 11:39:41 +0200

    petle_switch

---
+++ cpp/petle_znak.cpp
@@ -0,0 +1,23 @@
+/*
+ * main.cpp
+ *pobiera numer miesiaca i wyświetla jego nazwę 
+ * petla zaporowa
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    char zn= '';
+    
+     while (zn== t)
+    {
+       cout << "Podaj znak" << endl;
+       cin >> zn;
+    };
+    
+	return 0;
+}
+
