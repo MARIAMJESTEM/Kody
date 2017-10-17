86d51471efe7e3ea10e6c2dc4ee7d37b2ef6e078
MARIAMJESTEM <kl2ag2@komp01.lo1cg.org>
2017-10-17 12:04:20 +0200

    zakres

--- cpp/petle_znak.cpp
+++ cpp/petle_znak.cpp
@@ -1,6 +1,6 @@
 /*
  * main.cpp
- *pobiera numer miesiaca i wyświetla jego nazwę 
+ *p
  * petla zaporowa
  */
 
@@ -10,14 +10,21 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    char zn= '';
+    char zn= 't'; // musi być inicjacja jakimś znakiem 
     
-     while (zn== t)
+     while (zn== 't' || zn == 'n' || zn== 'T' || zn == 'N')
     {
        cout << "Podaj znak" << endl;
        cin >> zn;
     };
-    
+    //switch (zn)
+    //{
+        //case't' :
+        //case 'T':
+        //case 'N':
+        //case'n':
+           //cout << "Poprawna" << endl;
+        //}
 	return 0;
 }
 
