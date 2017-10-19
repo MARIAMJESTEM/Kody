6c8bf3b6138f0763aabd68fea97a65044134fd6a
MARIAMJESTEM <31763361+MARIAMJESTEM@users.noreply.github.com>
2017-10-19 21:54:35 +0200

    Add files via upload

--- cpp/petle_cw4_kl2g2_Mazur.cpp
+++ cpp/petle_cw4_kl2g2_Mazur.cpp
@@ -1,5 +1,7 @@
 /*
  * praca_domowa.cpp
+ * //w rob przechowujemy wartość bezwzględną z n
+ //(ponieważ liczba mogłabyć ujemna, a suma cyfr jest liczbą dodatnią)
  */
 
 
@@ -10,18 +12,18 @@ using namespace std;
 int main(int argc, char **argv)
 
 {
-int liczba = 0 ;
-int suma=0;
-cout<<"Podaj liczbe: "<<endl;
-cin>> liczba;
 
-	while(liczba > 0)
-	{
-		suma =suma +liczba %10;
-		liczba/=10;
-		cout<< "Suma : "<< suma <<liczba <<endl;
-    }
+int n;
+		cout << "Podaj liczbe calkowita";
+		cin >> n;
+ int rob=abs(n); 
+ int s=0; 
+		while(rob > 0) 
+ {
+		s += rob % 10; 
+		rob /= 10; 
+ }
+		cout << "Suma cyfr liczby " << n << "wynosi" << s << endl;
 
-	return 0;
+		return 0;
 }
-
