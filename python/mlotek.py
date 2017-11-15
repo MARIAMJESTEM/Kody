6fe3bdd2a833e6b305b9ecdad785be1b40dfe818
MARIAMJESTEM <kl2ag2@komp01.lo1cg.org>
2017-11-15 12:35:05 +0100

    potega

--- python/mlotek.py
+++ python/mlotek.py
@@ -8,7 +8,7 @@ def losuj(ileliczb, maksliczb):
     liczby = []
     ile = 0  # ilość wylosowanych liczb
     while ile < ileliczb:
-        # for i in range(ileliczb):
+         # for i in range(ileliczb):
         liczba = randint(0, maksliczb)
         if liczby.count(liczba) == 0:
             liczby.append(liczba)
@@ -18,24 +18,28 @@ def losuj(ileliczb, maksliczb):
 
 def pobierz_typy(ileliczb):
     typy = set()  # pusty zbiór
-    ile = 0  # ilość wylosowanych liczb
+    ile = 0  # ilość podanych typów
     while ile < ileliczb:
-        # for i in range(ileliczb):
-        typ = input("Podaj typ : ")
+        typ = input("Podaj typ")
         if typ not in typy:
             typy.add(typ)
             ile += 1
+
     return typy
 
 
 def main(args):
-    ileliczb = int(input("Ile liczb chcesz zgadnąć ?"))
-    maksliczb = int(input("Podaj górny zakres. "))
-    while ileliczb > maksliczb:
-        ileliczb = int(input("Ile liczb chcesz zgadnąć z %s ?" % maksliczb))
+    try:
+        ileliczb = int(input("Ile liczb chcesz zgadnąć ?"))
+        maksliczb = int(input("Podaj górny zakres. "))
+
+        while ileliczb > maksliczb:
+            ileliczb = int(input("Ile liczb chcesz zgadnąć z %s?" % maksliczb))
+            expect VALUEerror
 
     liczby = losuj(ileliczb, maksliczb)
     typy = pobierz_typy(ileliczb)
+
     print(liczby)
     print(typy)
     trafione = set(liczby) & typy
@@ -46,4 +50,4 @@ def main(args):
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
