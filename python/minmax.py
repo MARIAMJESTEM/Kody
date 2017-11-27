8262487b05eaae26ad30bca1816ac53eddad9c80
MARIAMJESTEM <mariamazur.2000@wp.pl>
2017-11-27 22:07:38 +0100

    minmax

--- python/minmax.py
+++ python/minmax.py
@@ -7,10 +7,19 @@ from random import randint
 
 
 def minmax(lista):
-    lmin = lista[0]
-    lmax = lista1[0]
-    for i in range(len(lista) / 2):
-
+     lmin = []
+     lmax = []
+     indeks = 0
+     for i in range(int(len(lista) / 2)):
+         if lista[indeks] > lista[indeks + 1]:
+             lmin.append(lista[indeks + 1])
+             lmax.append(lista[indeks])
+             indeks += 2
+         else:
+             lmin.append(lista[indeks])
+             lmax.append(lista[indeks + 1])
+             indeks += 2
+     return lmin, lmax
 
 def minimum(lista):
     min = lista[0]
