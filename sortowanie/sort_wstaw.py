348f6d112c9a3e2f8f13bf0e1b1bc12a936e7a06
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-01-10 12:36:33 +0100

    excel

--- sortowanie/sort_wstaw.py
+++ sortowanie/sort_wstaw.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from math import floor
+
 
 def sort_wstaw(lista):
     """ wersja liniowa"""
@@ -14,8 +16,9 @@ def sort_wstaw(lista):
     return lista
 
 
-def wyszukaj_binarnie_it(lista, el):
-    lewy, prawy = 0, len(lista) - 1
+
+def wyszukaj_binarnie(lewy, prawy, lista, el):
+    # lewy, prawy = 0, len(lista) - 1
     while lewy < prawy:
         srodek = floor((lewy + prawy) / 2)
         if el <= lista[srodek]:
@@ -27,17 +30,16 @@ def wyszukaj_binarnie_it(lista, el):
 # for (int 1=0; i < n)
 # n - to ilość elementów w tablicy
 
+
 def sort_wstaw_bin(lista):
     """ wersja binarna """
     for i in range(1, len(lista)):
         el = lista[i]
-        k = wyszukaj_bin(lewy, prawy, lista, el)
+        k = wyszukaj_binarnie(0, i, lista, el)
         # tworzenie listy z wstawionym elementem
         # todo
-        lista =
-
-
-
+        lista = lista[:k] + [el] + lista[k:i] + lista[i + 1:]
+        print(lista)
 
 
 def main(args):
@@ -46,7 +48,8 @@ def main(args):
     # f[3, 4, 7, 0, 2, 3, 1, 9]
     # s[3, 4, 7, 0, 2, 3, 1, 9]
     # t[ 0, 3, 4, 7, 2, 3, 1, 9]
-    print(sort_wstaw(lista))
+    # print(sort_wstaw(lista))
+    sort_wstaw_bin(lista)
     return 0
 
 
