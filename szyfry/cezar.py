94fa3cd40f96b73f2e12453ac7d6fa6be0007122
MARIAMJESTEM <mariamazur.2000@wp.pl>
2018-02-27 19:56:06 +0100

    zaszyfrowane dane

--- szyfry/cezar.py
+++ szyfry/cezar.py
@@ -2,35 +2,49 @@
 # -*- coding: utf-8 -*-
 
 
+
+
 def szyfruj(tekst, klucz):
-    '''
-    szyfrowanie tekstu za pomocą szyfru Cezara
-    '''
+    """Szyfrowanie tekstu za pomocą szyfru Cezara"""
+
     szyfrogram = ""
     klucz = klucz % 26
     for znak in tekst:
         ascii = ord(znak) + klucz
-        if ascii > 90:
+        if ord(znak) == 32:
+            ascii = 32
+        if ascii > 90 and ascii < 97:
+            ascii -= 26
+        elif ascii > 122:
             ascii -= 26
         szyfrogram += chr(ascii)
     return szyfrogram
 
 
+
 def deszyfruj(szyfrogram, klucz):
     tekst = ""
-    pass
+    for znak in szyfrogram:
+        ascii = ord(znak) - klucz
+        if ord(znak) == 32:
+            ascii = 32
+        if ascii > 90 and ascii < 97:
+            ascii -= 26
+        elif ascii > 122:
+            ascii -= 26
+        tekst += chr(ascii)
     return tekst
 
-    # obsłużyć małe i duże litery
-    # obsłużyć spacje
+# obsłużyć małe i duże litery
+# obsłużyć spacje
 
 
 def main(args):
     tekst = input("Podaj tekst: ")
     klucz = int(input("Podaj klucz: "))
     szyfrogram = szyfruj(tekst, klucz)
-    print (szyfrogram)
-    print (deszyfruj(szyfrogram, klucz))
+    print(szyfrogram)
+    print(deszyfruj(szyfrogram, klucz))
     return 0
 
 
