8d1c2164be3a57aa51a49501c7b476cc3ebce243
MARIAMJESTEM <mariamazur.2000@wp.pl>
2020-05-18 12:14:21 +0200

    proba

--- python/dodaj.py
+++ python/dodaj.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def sumuj(x, y):
-    return x + y 
-def odejmij(x, y):
-    return x - y 
-
-a= int(input(' Podaj liczbę'))
-b= int(input(' Podaj liczbę'))
-
-print ('Suma: ', sumuj(a ,b))
-print ('Róznica: ', odejmij(a ,b))
-
+print("czesc")
